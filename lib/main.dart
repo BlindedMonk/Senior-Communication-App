9e47e505cd5ec9ce538cd527322c5443afb4e23a
BlindedMonk <star31398@gmail.com>
2022-05-08 21:25:31 -0700

    finished adding firebase provider, lots of errors needing fixing though

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:senior_communication_aaron/authentication_service.dart';
 import 'package:senior_communication_aaron/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 
-void main() async {
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,22 +19,50 @@ class MyApp extends StatelessWidget {
 // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+            create: (_) => AuthenticationService(FirebaseAuth.instance),
         ),
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
-    );
+
+        StreamProvider(
+          create: (context) => context.read<AuthenticationService>().authStateChanges,
+        ),
+      ],
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            appBarTheme: AppBarTheme(
+              backgroundColor: Colors.white,
+              elevation: 0,
+            ),
+            primarySwatch: Colors.blue,
+          ),
+          home: LoginPage(),
+          //home: AuthenticationWrapper(),
+        ),);
   }
 
 }
+
+class AuthenticationWrapper extends StatelessWidget {
+  const AuthenticationWrapper({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if(firebaseUser != null){
+      return Text("Signed in");
+    }
+    return Text("Not signed in");
+  }
+}
+
 
 
 
